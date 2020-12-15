57f461f850b0307b325ecd0b07d5c94576c718b4
alexandrarusso <alexandrarusso@alexandras-mbp>
2020-12-14 21:38:38 -0500

    complete version

--- BankAccount2/src/BankAccount2.java
+++ BankAccount2/src/BankAccount2.java
@@ -2,7 +2,7 @@
 public class BankAccount2 {
 
 
-	private static double balance;
+	private double balance;
 	private double interestPct;
 		
 	/**
@@ -10,42 +10,49 @@ public class BankAccount2 {
 	 * @param newBalance 
 	 */
 
-		public BankAccount2(double newBalance, double intPct) {
-			balance = newBalance;
-			
+		public BankAccount2(double amt, double intPct) {
+			balance = amt;
+			interestPct = intPct;
 		}
-
 		
 		// opening balance for account
 	public void OpeningBalance (double open)	{
 		balance = balance + open;
 		}
 	
-	
-	public void threshold (double T)	{
-		balance = balance + T;
+	// makes a deposit
+	public void deposit (double amt )	{
+		balance = balance + amt;
+		System.out.println("You deposited" + amt);
+		System.out.println("Your new balance is" + balance);
 		}
 	
 	//interest that is added onto balance
 	public void calcInterest() {
-		
 		balance = balance + (interestPct*balance);
-			
 		}
 	
-	public double getBalance() 
+	// makes a withdrawal
+	public void withdraw(double amt) 
 	{
+		// if you try to take out more money than what is in the account you will get a message
+		if (amt > balance) {
+			System.out.println("Insufficient funds to support withdrawal");
+		}
+		else {
+			balance = balance - amt;
+			System.out.println("You took out $:" +amt);
+			System.out.println("Your new balance is " +balance);
+		}
+	}
+	
+	public double getBalance() {
 		return balance;
 	}
 	
-	
-	public static void main(String[] args) {
-		
-		BankAccount2 obj = new BankAccount2(balance, balance);
-		
-		obj.OpeningBalance(1000);
-		obj.threshold(1500);
-		
-		
+	public void originalBal(double original) {
+		balance = original;
 	}
+		
+
 }
--- BankAccount2/src/BankThreshold.java
+++ BankAccount2/src/BankThreshold.java
@@ -1,32 +1,42 @@
-import javax.swing.JOptionPane;
 
+import java.util.Scanner;
 public class BankThreshold {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
-		// threshold to reach is 1500
-		String AB=JOptionPane.showInputDialog("Enter the threshold for your account balance reach");
-		
-		//AccountBalance=Integer.parseInt(JOptionPane.showInputDialog(null, "Enter the threshold for your account balance reach"));
-		
-		
+		 Scanner Alex = new Scanner(System.in);
+		 
 		// 1000
-		String OB=JOptionPane.showInputDialog("Enter opening balance");
-		
-		//OpeningBalance=Integer.parseInt(JOptionPane.showInputDialog(null, "Enter opening balance"));
+		System.out.println("Enter opening balance");
+		double original = Alex.nextDouble();
 		
 		// interest rate 0.5
-		String Interest=JOptionPane.showInputDialog("Enter interest rate");
+	System.out.println("Enter interest rate");
+		double openInt = Alex.nextDouble();
 		
 		
-		//Interest=Integer.parseInt(JOptionPane.showInputDialog(null, "Enter interest rate"));
+		BankAccount2 acct = new BankAccount2(original,openInt);
+				
+		System.out.println("Enter a threshold or type Q to quit ") ;
+	// outside while loop
+		while (Alex.hasNextDouble()) {
+			acct.originalBal(original);
+			
+			int months = 0;
+			double number = Alex.nextDouble();
 		
-		// repeats until user enters END
+// inisde nested while loop
+		while (acct.getBalance() < number) {
+			acct.calcInterest();
+			months++;
+		}
+		System.out.println("It will take " +months+ " months to reach the threshold");
+		System.out.println("Enter a threshold:");
 	
-		
-		System.out.println("It will take x months to reach the threshold");
+		}
+		acct.originalBal(original);
+		System.out.println("Original balance restored: " +acct.getBalance());
 	}
 
 }
